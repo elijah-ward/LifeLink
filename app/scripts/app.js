75990d7826525fbb0f5d83492ca710aa05a791b6
Elijah Ward <elijahward.dev@gmail.com>
2016-10-15 00:41:13 -0400

    Added more bootstrap structure

---
+++ app/scripts/app.js
@@ -0,0 +1,38 @@
+(function () {
+    'use strict';
+    
+    var _templateBase = './templates';
+    
+    angular.module('app', [
+        'ngRoute',
+        'ngMaterial',
+        'ngAnimate',
+        'ngResource'
+    ])
+    .config(['$routeProvider','$mdThemingProvider', '$mdIconProvider', function ($routeProvider, $mdThemingProvider, $mdIconProvider) {
+
+
+            $mdThemingProvider.theme('default')
+            .primaryPalette('grey')
+    .accentPalette('indigo')
+    .warnPalette('grey');
+            $mdThemingProvider.theme('dash-dark').primaryPalette('amber').dark();
+            $mdThemingProvider.theme('card-grey').backgroundPalette('grey');
+            $mdThemingProvider.theme('dark-orange').backgroundPalette('orange').dark();
+            $mdThemingProvider.theme('dark-purple').backgroundPalette('deep-purple').dark();
+            $mdThemingProvider.theme('dark-blue').backgroundPalette('blue').dark();
+            $mdThemingProvider.theme('dark-blue-grey').backgroundPalette('blue-grey').dark();
+
+
+            $routeProvider.when('/', {
+                templateUrl: _templateBase + '/dash/dash.html' ,
+                controller: 'DashController',
+                controllerAs: '_authctrl'
+            });
+
+            $routeProvider.otherwise({ redirectTo: '/' });
+        }
+        ]);
+    
+
+})();
