d23af3f66199756162342d936aa98c19fe244913
Elijah Ward <elijahward.dev@gmail.com>
2016-10-16 09:54:01 -0400

    last commit before submit

--- server/app.js
+++ server/app.js
@@ -12,6 +12,75 @@ var unameGen = require('username-generator');
 var moment = require('moment');
 moment().format();
 
+var indico = require('indico.io');
+indico.apiKey =  '26cec65464998714f4d824f9511c8adc';
+
+var response = function(res) { console.log(res); }
+var logError = function(err) { console.log(err); }
+
+
+/*//////////////////////ANALYTICS DATA////////////////*/
+
+
+
+///Constants///
+
+var londonLong = '42.9870';
+var londonLat = '-81.2432';
+var londonRad = '10km';
+
+
+
+////////////////TWITTER API/////////////////////
+
+var twitter_credentials = {
+  key : '3uKYXpMZ0zmUQmVH3gEuMd2g7',
+  secret : 'jx1y3cCGtHhGPd9J5w6gvMdSUN2o59gZf8QKbhB6WcGwzkWRQm',
+  token : '3695892973-aSaz6hlnSHjpMay6z0TsAtLwYhkji56oKb9lOsK',
+  access_secret : 'D7tLEjzhWt1bhSVITiutx2G8Mp1MGGKQLznAO5XdRFmtl'
+};
+
+
+
+
+
+///////////////INDICIO.IO API CREDS ARE ABOVE/////////////////
+
+
+
+
+
+
+///////////////DICTIONARY API////////////////
+
+var dictionary_credentials = {
+  api_key : 'a619c4e7-ac9b-4ef8-90c1-e56f443aa7d2'
+};
+
+
+
+//////////////THESAURUS API STILL DICTIONARYAPI.COM//////////////////
+
+var thesaurus_credentials = {
+  api_key : '29b00fa8-05fe-40ce-b8ca-fd021981a495'
+};
+
+
+
+// single example
+// indico.sentimentHQ("I love writing code!")
+//   .then(response)
+//   .catch(logError);
+
+// batch example
+// var batchInput = [
+//     "I love writing code!",
+//     "Alexander and the Terrible, Horrible, No Good, Very Bad Day"
+// ];
+// indico.sentimentHQ(batchInput)
+//   .then(response)
+//   .catch(logError);
+
 app.use(express.static('../app/'));
 
 mongoose.connect("mongodb://127.0.0.1:27017/lifelink");
@@ -49,43 +118,6 @@ app.get('/', function(req, res) {
   res.sendFile(path.resolve('../app/index.html'));
 });
 
-//This route is simply run only on first launch just to generate some chat history
-app.post('/setup', function(req, res) {
-  //Array of chat data. Each object properties must match the schema object properties
-  var chatData = [{
-    created: new Date(),
-    content: 'Hi',
-    username: 'Chris',
-    room: 'php'
-  }, {
-    created: new Date(),
-    content: 'Hello',
-    username: 'Obinna',
-    room: 'laravel'
-  }, {
-    created: new Date(),
-    content: 'Ait',
-    username: 'Bill',
-    room: 'angular'
-  }, {
-    created: new Date(),
-    content: 'Amazing room',
-    username: 'Patience',
-    room: 'socet.io'
-  }];
-
-  //Loop through each of the chat data and insert into the database
-  for (var c = 0; c < chatData.length; c++) {
-    //Create an instance of the chat model
-    var newChat = new Chat(chatData[c]);
-    //Call save to insert the chat
-    newChat.save(function(err, savedChat) {
-      console.log(savedChat);
-    });
-  }
-  //Send a resoponse so the serve would not get stuck
-  res.send('created');
-});
 
 //This route produces a list of chat as filterd by 'room' query
 app.get('/msg', function(req, res) {
@@ -177,23 +209,22 @@ io.on('connection', function(socket) {
       io.in(msg.room).emit('message created', msg);
     });
   });
+
+  var data = 'aloha';
+
+app.get('https://api.twitter.com/1.1/search/tweets.json' + 'q=@' + data + 'geocode=' + londonLong+', '+londonLat+', '+londonRad, function(req,res){
+
+console.log(res);
+
+
+});
+
+
 });
 /*||||||||||||||||||||END SOCKETS||||||||||||||||||*/
 
 
 
-/*//////////////////////ANALYTICS////////////////*/
-
-
-
-////////////////TWITTER API/////////////////////
-
-
-
-
-
-
-///////////////INDICIO.IO API/////////////////
 
 
 
@@ -201,15 +232,8 @@ io.on('connection', function(socket) {
 
 
 
-///////////////DICTIONARY API////////////////
 
-
-
-
-
-
-//////////////THESAURUS API//////////////////
-
+//start the server
 
 server.listen(3000);
 console.log('Server listening on port '+'3000');
